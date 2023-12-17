2df5ddaf6d7160dd67157a29a3224f3b949916d9
Tiago Teixeira <101202571+T-yago@users.noreply.github.com>
2023-12-17 20:06:37 +0000

    Added DNS files

--- DNS_conf_files/named.conf.local.ts
+++ DNS_conf_files/named.conf.local.ts
@@ -1,9 +1,14 @@
 // Do any local configuration here
 // não esquecer que isto implica criar a pasta zones e o ficheiro CC2023.zone
 
-zone "CC2023" {
-        type master;
-        file "etc/bind/zones/CC2023.zone";
+zone "cc2023"{
+	type master;
+	file "/etc/bind/zones/db.cc2023";
+};
+
+zone "10.in-addr.arpa" {
+	type master;
+	file "/etc/bind/zones/db.10";
 };
 
 //
@@ -11,6 +16,5 @@ zone "CC2023" {
 // Consider adding the 1918 zones here, if they are not used in your
 // organization
 //include "/etc/bind/zones.rfc1918";
-
 
 
